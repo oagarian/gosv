a6b08eec1a5616e3e2ff5a7a2a339dc5d48ea1cc
oagarian <tns6@aluno.ifal.edu.br>
2024-07-03 14:35:31 -0300

    feat: convert csv to json

--- app/action/convert.go
+++ app/action/convert.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
@@ -75,6 +76,57 @@ func convertCSVtoJSON(header []string, inputPath, separator string) error {
 	return nil
 }
 
+func convertCSVtoSQL(header []string, inputPath, separator string) error {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return fmt.Errorf("error while opening file: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = []rune(separator)[0] // Convertendo para rune
+	reader.LazyQuotes = true
+
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return fmt.Errorf("error while reading .csv: %v", err)
+	}
+
+	if len(rows) == 0 {
+		return fmt.Errorf("empty CSV file")
+	}
+
+	tableName := getTableName(inputPath)
+
+	var sqlStatements []string
+
+	for _, row := range rows[1:] { // Ignorando o cabeçalho ao criar as instruções SQL
+		values := make([]string, len(header))
+		for i, value := range row {
+			values[i] = fmt.Sprintf("'%s'", escapeSingleQuotes(value))
+		}
+		sql := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s);", tableName, strings.Join(header, ", "), strings.Join(values, ", "))
+		sqlStatements = append(sqlStatements, sql)
+	}
+
+	outputFileName := strings.TrimSuffix(inputPath, filepath.Ext(inputPath)) + ".sql"
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return fmt.Errorf("error creating output file: %v", err)
+	}
+	defer outputFile.Close()
+
+	for _, sql := range sqlStatements {
+		_, err := outputFile.WriteString(sql + "\n")
+		if err != nil {
+			return fmt.Errorf("error writing SQL statements to file: %v", err)
+		}
+	}
+
+	fmt.Printf("CSV converted to SQL insert statements and saved to %s\n", outputFileName)
+	return nil
+}
+
 func detectType(value string) (interface{}, error) {
 	if boolValue, err := strconv.ParseBool(value); err == nil {
 		return boolValue, nil
@@ -91,11 +143,25 @@ func equalSlice(a, b []string) bool {
 	return slices.Equal(a, b)
 }
 
-func Convert(header, inputPath, separator string) error {
+func getTableName(filePath string) string {
+	fileName := filepath.Base(filePath)
+	tableName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+	return tableName
+}
+
+func escapeSingleQuotes(value string) string {
+	return strings.ReplaceAll(value, "'", "''")
+}
+
+func Convert(header, inputPath, separator, outputExt string) error {
 	_header, err := splitHeader(header)
 	if err != nil {
         return err
     }
-	convertCSVtoJSON(_header, inputPath, separator)
+	if strings.ToLower(outputExt) == "json" {
+		convertCSVtoJSON(_header, inputPath, separator)
+	} else if strings.ToLower(outputExt) == "sql" {
+		convertCSVtoSQL(_header, inputPath, separator)
+	}
 	return nil
 }
